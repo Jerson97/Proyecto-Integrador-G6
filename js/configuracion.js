258244f46b3d7ab132ba21a8261f7e0c572416d8
Jerson97 <jersonramirezs97@gmail.com>
2022-11-26 18:12:39 -0500

    se agrego base de datos de contactenos al firebase

---
+++ js/configuracion.js
@@ -0,0 +1,17 @@
+
+
+// Initialize Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyBoLQduytd-frAULPOg4daRLmLhuo-Ri7M",
+    authDomain: "dataeventia.firebaseapp.com",
+    projectId: "dataeventia",
+    storageBucket: "dataeventia.appspot.com",
+    messagingSenderId: "180626148144",
+    appId: "1:180626148144:web:7d6a45687ec66e202f96f5"
+});
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = firebase.firestore();
+
+
---
+++ js/guardar.js
@@ -0,0 +1,16 @@
+function guardar() {
+    db.collection("usuarios").add({
+        nombre: document.getElementById('nombre').value,
+        apellidos: document.getElementById('apellido').value,
+        direccion: document.getElementById('inputAddress').value,
+        correo: document.getElementById('exampleInputEmail1').value,
+        telefono: document.getElementById('formGroupExampleInput').value
+
+    })
+    .then((docRef) => {
+        alert("registro exitoso")
+    })
+    .catch((error) => {
+        alert("Error en el registro")
+    });
+}
